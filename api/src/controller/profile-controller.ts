b750feaf091f1d2e531edc283e8588c23e563998
Hugo Boissiere <boissi_h@etna-alternance.net>
2019-02-21 15:39:43 +0100

    ouais

--- api/src/controller/profile-controller.ts
+++ api/src/controller/profile-controller.ts
@@ -4,15 +4,6 @@ import { Socket } from 'socket.io';
 import * as jwt_decode from 'jwt-decode'
 import { UserToken } from '../interfaces/auth-interfaces';
 import { GetProfileRequest, GetProfileResponse } from '../interfaces/profile-interfaces';
-import {
-	SearchUserRequest,
-	SearchUserResponse,
-	UpdateItem,
-	UpdateItemRequest,
-	UpdateItemResponse
-} from "../interfaces/list-interfaces";
-import { Item } from "../entity/Item";
-
 
 export async function getProfile(data: GetProfileRequest, socket: Socket) {
 	let responce: GetProfileResponse;
@@ -35,19 +26,4 @@ export async function getProfile(data: GetProfileRequest, socket: Socket) {
 		socket.emit('get-profile', responce);
 		return;
 	}
-}
-
-export async function searchUser(getUser: User, data: SearchUserRequest, socket: Socket) {
-	const connection: Connection = getConnection()
-	let response: SearchUserResponse = { code: 200, status: "ok", users: [] }
-	let userRep = await connection.getRepository(User)
-	let users = await userRep.find({ email: Like("%" + data.research + "%") })
-	users.forEach((user) => {
-		if (user.id == getUser.id)
-				return;
-		let formatUser: UserToken = { id: user.id, email: user.email, username: user.username };
-		response.users.push(formatUser);
-	});
-	socket.emit('search-user', response)
-	return;
 }
